be49cd1989a876241d95291ebd8ae6e0ccf9df38
Caio Costa <caiocrrodrigues2004@gmail.com>
2020-10-13 21:17:31 -0300

    Changed prototype design & quit mechanic

---
+++ PixelAdventure.EXE/Assets/Scripts/Prototype/JumpLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpLevel : MonoBehaviour
+{
+    void Start()
+    {
+        // After closing the game, will load scene Level_9
+        if (PlayerPrefs.GetInt("AfterQuit", 0) == 1)
+        {
+            SceneManager.LoadScene(8);
+        }
+    }
+}
--- PixelAdventure.EXE/Assets/Scripts/Prototype/PrototypeSpawnTrigger.cs
+++ PixelAdventure.EXE/Assets/Scripts/Prototype/PrototypeSpawnTrigger.cs
@@ -6,11 +6,16 @@ public class PrototypeSpawnTrigger : MonoBehaviour
 {
 
     private GameObject prototype;
+    private GameObject appleUI;
+    private GameObject scoreUI;
 
     void Start()
     {
         prototype = GameObject.Find("Prototype");
         prototype.SetActive(false);
+
+        appleUI = GameObject.Find("Canvas/Apple UI");
+        scoreUI = GameObject.Find("Canvas/Score");
     }
 
     void OnTriggerEnter2D(Collider2D collider)
@@ -18,6 +23,9 @@ public class PrototypeSpawnTrigger : MonoBehaviour
         if (collider.gameObject.tag == "Player")
         {
             prototype.SetActive(true);
+
+            appleUI.SetActive(false);
+            scoreUI.SetActive(false);
         }
     }
 
